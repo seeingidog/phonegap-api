cd79355dcc8f9a50c5853ad41f69d6d9b69aa4a0
Ian Morgan <ian@ruby-code.com>
2012-04-09 04:44:08 -0400

    Flesh out other http verbs to be used in write methods

--- lib/phonegap-api/base.rb
+++ lib/phonegap-api/base.rb
@@ -1,6 +1,6 @@
 module Phonegap
-  class UnsupportedPlatformError
-  end
+  class UnsupportedPlatformError < StandardError ; end
+  
   class Connection
     include HTTParty
     base_uri 'https://build.phonegap.com/api/v1'
@@ -14,6 +14,18 @@ module Phonegap
     
     def get(url)
       self.class.get(url, @auth).parsed_response
+    end
+    
+    def post(url, body)
+      self.class.post(url, @auth.merge!({:data => body})).parsed_response
+    end
+    
+    def put(url, body)
+      self.class.put(url, @auth.merge!({:data => body})).parsed_response
+    end
+    
+    def delete(url, body)
+      self.class.delete(url, @auth).parsed_response
     end
     
   end
---
+++ lib/phonegap-api/write.rb
@@ -0,0 +1,7 @@
+module Phonegap
+  
+  class Connection
+    def create_app()
+    end
+  end
+end
