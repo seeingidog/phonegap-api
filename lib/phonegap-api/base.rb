fcf2436814b8d5588782fc2439777687b6b5eaf0
Ian Morgan <ian@ruby-code.com>
2012-04-10 02:54:36 -0400

    Reorganize tests, refactor base requests

--- lib/phonegap-api/base.rb
+++ lib/phonegap-api/base.rb
@@ -13,27 +13,28 @@ module Phonegap
     end
     
     def get(url)
-      output = self.class.get(url, @auth).parsed_response
-      check_response!(output)
+      output = self.class.get(url, @auth)
+      check_response!(output).parsed_response
     end
     
     def post(url, body)
-      output = self.class.post(url, @auth.merge!({:body =>{:data => body}})).parsed_response
-      check_response!(output)
+      output = self.class.post(url, @auth.merge!({:body =>{:data => body}}))
+      check_response!(output).parsed_response
     end
     
     def put(url, body)
-      output = self.class.put(url, @auth.merge!({:body =>{:data => body}})).parsed_response
-      check_response!(output)
+      output = self.class.put(url, @auth.merge!({:body =>{:data => body}}))
+      check_response!(output).parsed_response
     end
     
     def delete(url)
-      output = self.class.delete(url, @auth).parsed_response
-      check_response!(output)
+      output = self.class.delete(url, @auth)
+      check_response!(output).parsed_response
     end
     
     def check_response!(output)
       raise APIError, output['error'] if output['error'].class == String
+      
       output
     end
   end
--- spec/phonegap-api_spec.rb
+++ spec/phonegap-api_spec.rb
@@ -6,51 +6,61 @@ describe "PhonegapApi" do
     @username = ""
     @password = ""
     @conn = Phonegap::Connection.new(@username, @password)
+    @specific_app_id = 94659
   end
   
-  it "recieves information about current user" do  
-    @conn.me.keys.should == ["apps", "keys", "username", "id", "link", "email"]
-  end
   
-  it "retrieves list of keys for account" do
-    @conn.keys.class.should == Hash
-  end
-  
-  it "gets list of apps" do
-    @conn.apps.class.should == Phonegap::Apps
-  end
-  
-  it "retrieves information about a specific app" do
-    @conn.get_app(94659).class.should == Phonegap::App
-  end
-  
-  it "retrieves the URL to the app icon" do
-    @conn.get_icon(94659)["location"].class.should == String
-  end
-  
-  it "retrieves the URL to the app icon" do
-    @conn.get_package_url(94659, "android").class.should == String
-  end
-  
-  it "returns an App object from API return data" do
-    @conn.get_app(94659).class.should == Phonegap::App
-  end
-  
-  it "returns json usable by the API from an App object" do
-    obj = @conn.get_app(94659)
-    obj.to_json.class.should == String
-  end
-  
-  it "returns an Apps object composed of individual apps based on API return data" do
-    @conn.apps.class.should == Phonegap::Apps
-  end
-  
-  it "creates an app" do
-    @app_id = @conn.create_app(:title => 'API test app', :repo => 'https://github.com/alunny/phonegap-start.git', :create_method => 'remote_repo')['id']
-  end
+  describe "Read operations" do
     
-  it "deletes the created app" do
-    @conn.delete_app(@app_id)
+    it "recieves information about current user" do  
+      @conn.me.keys.should == ["apps", "keys", "username", "id", "link", "email"]
+    end
+
+    it "retrieves list of keys for account" do
+      @conn.keys.class.should == Hash
+    end
+
+    it "gets list of apps" do
+      @conn.apps.class.should == Phonegap::Apps
+    end
+
+    it "retrieves information about a specific app" do
+      @conn.get_app(@specific_app_id).class.should == Phonegap::App
+    end
+
+    it "retrieves the URL to the app icon" do
+      @conn.get_icon(@specific_app_id)["location"].class.should == String
+    end
+
+    it "retrieves the URL to the app icon" do
+      @conn.get_package_url(@specific_app_id, "android").class.should == String
+    end
+
+    it "returns an App object from API return data" do
+      @conn.get_app(@specific_app_id).class.should == Phonegap::App
+    end
+
+    it "returns json usable by the API from an App object" do
+      obj = @conn.get_app(@specific_app_id)
+      obj.to_json.class.should == String
+    end
+    
   end
   
+  describe "Write operations" do
+  
+    it "returns an Apps object composed of individual apps based on API return data" do
+      @conn.apps.class.should == Phonegap::Apps
+    end
+  
+    it "creates an app" do
+      @app_id = @conn.create_app(:title => 'API test app', :repo => 'https://github.com/alunny/phonegap-start.git', :create_method => 'remote_repo')['id']
+    end
+    
+    it "deletes the created app" do
+      @conn.delete_app(@app_id)
+    end
+
+  end  
+
 end
